8a9af336b67f368b5c8586b20c0694816ff0ad75
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-07-28 16:27:03 +0200

    pen stroke option

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 let myCanvas = document.getElementById('myCanvas');
 let ctx = myCanvas.getContext('2d');
 if (myCanvas.getContext) {
-    const ctx = myCanvas.getContext("2d");}
-
+    const ctx = myCanvas.getContext("2d");
+}
 const colorPicker = document.getElementById('color');
 const sizePicker = document.getElementById('size');
-const tea = document.getElementById('tea');
-const pc = document.getElementById('pc');
-const globe = document.getElementById('globe');
-const data = document.getElementById('data');
-const globe2 = document.getElementById('globe2');
-const message2 = document.getElementById('message2');
-const message1 = document.getElementById('message1');
+
+const pen = document.getElementById('pen');
+let usePen = false;
+pen.addEventListener('click', () => {
+    usePen = true;
+});
 
 let src = '';
 let icons = document.querySelectorAll('.image-icon')
 icons.forEach(function (icon) {
-icon.addEventListener('click', (event)=> {
-    icon.classList.add('active');
-    src = icon.getAttribute('src');
-    let otherIcons = Array.from(icons).filter(function(icon){
-        return icon !== event.target
+    icon.addEventListener('click', (event) => {
+        icon.classList.add('active');
+        usePen = false;
+        src = icon.getAttribute('src');
+        let otherIcons = Array.from(icons).filter(function (icon) {
+            return icon !== event.target
+        })
+        console.log(src);
+        otherIcons.forEach(function (icon) {
+            icon.classList.remove('active');
+        })
+        return src;
     })
-console.log(src);
-    otherIcons.forEach(function (icon) {
-        icon.classList.remove('active');
-    })
-    return src;
-})
-} );
+});
 
 
 let painting = false;
@@ -40,8 +40,8 @@ function startPosition(e) {
 
 function endPosition() {
     painting = false;
-    ctx.beginPath();  
-} 
+    ctx.beginPath();
+}
 
 /* function draw(e) {
     if (!painting) return;
@@ -62,30 +62,46 @@ function endPosition() {
     
     console.log(x);
 } */
-    const img = new Image();
-    function draw(e) {
-        
+const img = new Image();
+function draw(e) {
+
     if (!painting) return;
-        const rect = myCanvas.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        
-        ctx.drawImage(img, x, y);
-        img.src = src;
+    img.src = src;
+    const rect = myCanvas.getBoundingClientRect();
+    const x = e.clientX - rect.left - 60;
+    const y = e.clientY - rect.top - 60;
+
+    ctx.drawImage(img, x, y);
+
+    if(usePen){
+        if(sizePicker && colorPicker) {
+            ctx.lineWidth = sizePicker.value;
+            ctx.strokeStyle = colorPicker.value;
+        }
+       
+        ctx.lineCap = 'round';
+         
+        ctx.lineTo(x, y); //disegna una linea
+        ctx.stroke(); //traccia il percorso
+        ctx.beginPath();//inizia un percorso
+        ctx.moveTo(x, y); //muove il pennello a x y
     }
-    
+   
+
+}
+
 
 myCanvas.addEventListener('mousedown', startPosition);
 myCanvas.addEventListener('mouseup', endPosition);
 myCanvas.addEventListener('mousemove', draw);
 myCanvas.addEventListener('mousemove', (e) => {
-console.log(e.clientX);
-const rect = myCanvas.getBoundingClientRect();
-let Cx = e.clientX - rect.left;
-let Cy = e.clientY - rect.top;
-let x = document.getElementById('x');
-let y = document.getElementById('y');
-x.textContent = 'X: '+ Cx;
-y.textContent = 'Y: ' + Cy;
+    console.log(e.clientX);
+    const rect = myCanvas.getBoundingClientRect();
+    let Cx = e.clientX - rect.left;
+    let Cy = e.clientY - rect.top;
+    let x = document.getElementById('x');
+    let y = document.getElementById('y');
+    x.textContent = 'X: ' + Cx;
+    y.textContent = 'Y: ' + Cy;
 })
 
