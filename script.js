f9f467fa3f3d3604ce45fcb8d024f6b5b4a64d18
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-07-28 02:21:46 +0200

    add css inline to avoid distortion in canvas tag

--- script.js
+++ script.js
@@ -18,17 +18,33 @@ function endPosition() {
 function draw(e) {
     if (!painting) return;
 
+    const rect = myCanvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    console.log(x);
+
     ctx.lineWidth = sizePicker.value;
     ctx.lineCap = 'round';
     ctx.strokeStyle = colorPicker.value;
 
-    ctx.lineTo(e.clientX - myCanvas.offsetLeft, e.clientY - myCanvas.offsetTop);
+    ctx.lineTo(x, y);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(e.clientX - myCanvas.offsetLeft, e.clientY - myCanvas.offsetTop);
+    ctx.moveTo(x, y);
+    console.log(x);
 }
 
 myCanvas.addEventListener('mousedown', startPosition);
 myCanvas.addEventListener('mouseup', endPosition);
 myCanvas.addEventListener('mousemove', draw);
+myCanvas.addEventListener('mousemove', (e) => {
+console.log(e.clientX);
+const rect = myCanvas.getBoundingClientRect();
+let Cx = e.clientX - rect.left;
+let Cy = e.clientY - rect.top;
+let x = document.getElementById('x');
+let y = document.getElementById('y');
+x.textContent = 'X: '+ Cx;
+y.textContent = 'Y: ' + Cy;
+})
 
